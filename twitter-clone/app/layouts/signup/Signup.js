d5a71e2a647f5bd4e1dd0e1e8025009fd457b301
Felicia Garcia <feliciagarcia@Felicias-MacBook-Pro.local>
2017-08-15 16:35:48 -0400

    added imports for signup layout

---
+++ twitter-clone/app/layouts/signup/Signup.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ImagePicker } from 'expo';
+import { Actions } from 'react-native-router-flux';
+import {View} from 'react-native';
+import {
+  Container,
+  Content,
+  Button,
+  Text,
+  Form,
+  Thumbnail,
+  Icon
+} from 'native-base';
+import axios from 'axios';
+import TextField from '../../components/TextField';
+import styles from './styles';
+import { addUser } from '../../redux/reducers/users';
+import cosmicConfig from '../../config/cosmic';
+const mapDispatchToProps = {addUser};
+const validate = form => {
+  let errorMessage = '';
+  if (form.username.includes(" ")){
+    errorMessage = "Username cannot contain spaces";
+  }
+  if (form.password.includes(" ")){
+    errorMessage = "Password cannot contain spaces";
+  }
+  Object.keys(form).slice(0, 5).map(field => {
+    if (!form[field]){
+      errorMessage = 'All fields must be filled';
+    }
+  })
+  return errorMessage;
+}
+class Signup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      username: '',
+      password: '',
+      image: null,
+      error: '',
+    };
+  }
