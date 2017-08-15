bd3f25ecde43103e7cd759f629c9504d6e07bb70
Felicia Garcia <feliciagarcia@Felicias-MacBook-Pro.local>
2017-08-15 16:03:33 -0400

    started constructor in app.js file

--- twitter-clone/App.js
+++ twitter-clone/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import { Provider, connect } from 'react-redux';
+import { Font, AppLoading } from 'expo';
+import store from './app/redux/store';
+import Router from './app/config/routes';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      isReady: false,
+    }
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
