422eae603cdc7020f5ff566bfe2521b1950329e5
Felicia Garcia <feliciagarcia@Felicias-MacBook-Pro.local>
2017-08-15 17:29:55 -0400

    doesn't work yet

--- twitter-clone/app/redux/reducers/posts.js
+++ twitter-clone/app/redux/reducers/posts.js
@@ -5,3 +5,24 @@ export const loadPosts = () => dispatch => {
     .then(sortedPosts => dispatch(init(sortedPosts)))
     .catch(err => console.error(`Could not load posts`, err));
 };
+
+export const createPost = post => dispatch => {
+  return axios.post(`https://api.cosmicjs.com/v1/${cosmicConfig.bucket.slug}/add-object`, {
+      title: post.user.username + ' post',
+      type_slug: 'posts',
+      content: post.content,
+      metafields: [
+        {
+          type: 'object',
+          title: 'User',
+          key: 'user',
+          object_type: 'users',
+          value: post.user.id
+        },
+      ]
+    })
+      .then(res => formatPost(res.data, post))
+      .then(formattedPost => dispatch(create(formattedPost)))
+      .then(() => Actions.feed({type: 'popAndReplace'}))
+      .catch(error => console.error('Post unsuccessful', error))
+}
