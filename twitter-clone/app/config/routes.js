23d297dd94debb39bafd2cfa42414c78b960993b
Felicia Garcia <feliciagarcia@Felicias-MacBook-Pro.local>
2017-08-15 16:23:40 -0400

    added imports in the router.js

---
+++ twitter-clone/app/config/routes.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Scene, Router, Actions, ActionConst } from 'react-native-router-flux';
+import Welcome from '../layouts/welcome';
+import Login from '../layouts/login';
+import Signup from '../layouts/signup';
+import NewPost from '../layouts/newPost';
+import Feed from '../layouts/feed';
