f3f43fdb7f3f7af3c4fdc080563faa4c67d027cd
Felicia Garcia <feliciagarcia@Felicias-MacBook-Pro.local>
2017-08-15 16:38:31 -0400

    added form and image upload function

--- twitter-clone/app/layouts/signup/Signup.js
+++ twitter-clone/app/layouts/signup/Signup.js
@@ -65,3 +65,82 @@ checkUsername(username){
     }
   })
 }
+uploadImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
+  render(){
+    return (
+      <Container style={styles.container}>
+        <Content>
+          <Form style={styles.mar10}>
+            <TextField
+              name="First Name"
+              value={this.state.firstName}
+              onChangeText={(text) => this.setState({firstName: text})}
+            />
+            <TextField
+              name="Last Name"
+              value={this.state.lastName}
+              onChangeText={(text) => this.setState({lastName: text})}
+            />
+            <TextField
+              name="Username"
+              value={this.state.username}
+              onChangeText={(text) => this.setState({username: text})}
+            />
+            <TextField
+              secureTextEntry
+              name="Password"
+              value={this.state.password}
+              onChangeText={(text) => this.setState({password: text})}
+            />
+          </Form>
+          <Text style={styles.addPic}>Add a profile picture</Text>
+          {
+            !this.state.image &&
+            <Button
+              primary
+              bordered
+              onPress={this.uploadImage}
+              style={styles.uploadButton}>
+              <Icon
+                ios='ios-camera'
+                android='md-camera'
+              />
+            </Button>
+          }
+          {
+            this.state.image &&
+            <Thumbnail
+              size={80}
+              source={{uri: this.state.image}}
+              style={styles.thumbnail}
+            />
+          }
+          <Button
+            block
+            style={styles.mar10}
+            onPress={() => this.onSubmit()}
+          >
+            <Text>Create account</Text>
+          </Button>
+          <Text style={styles.formMsg}>{this.state.error}</Text>
+          <Button
+            transparent
+            style={styles.loginBtn}
+            onPress={() => Actions.login()}
+          >
+            <Text style={styles.loginTxt}>Already have an account?</Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
+}
+export default connect(null, mapDispatchToProps)(Signup);
